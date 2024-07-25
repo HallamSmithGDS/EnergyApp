31b2052dafcd3b0d029155a3e4f127b6c4495c6b
HallamSmithGDS <hallam.smith@gds.uk.com>
2024-07-25 11:03:00 +0100

    update

--- CalcApp.py
+++ CalcApp.py
@@ -219,4 +219,4 @@ if st.button("Calculate"):
     st.write('Annual Electricity Bill Reduction:', CostSaving)
     st.write('Annual CO2 Reduction: ', CO2Saving , 'Tonnes')
     st.write('')
-    st.bar_chart(Carbon_df, x = 'Name', x_label = "Tonnes Per Year", y_label = "CO2 Emissions", horizontal=True, height = 100)
+    st.bar_chart(Carbon_df, x = 'Name', x_label = "Tonnes Per Year", y_label = "CO2 Emissions", horizontal=True, height = 200)
