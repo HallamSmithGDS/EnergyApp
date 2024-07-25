e07a6ccb214289c4bd5c9d13bdab71c5c60614b8
HallamSmithGDS <hallam.smith@gds.uk.com>
2024-07-25 16:42:17 +0100

    update

--- CalcApp.py
+++ CalcApp.py
@@ -292,7 +292,7 @@ if st.button("Calculate"):
     if PaybackOn:
         st.divider()
         st.subheader("Payback Calculations")
-        st.write(Payback_df(index = false))
+        st.write(Payback_df(index = False))
         st.write('')
         if exact_year is not None:
             payback_years = int(exact_year)
